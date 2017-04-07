fa9c18a4e495a6dcc043fce363b10d4bad932663
jschmuland <joelschmuland@gmail.com>
2017-04-07 17:04:46 -0400

    Added the header to the classes created feel free to replace classes as needed

--- src/symphony/domain/Date.java
+++ src/symphony/domain/Date.java
@@ -1,5 +1,25 @@
 package symphony.domain;
 
+/**
+* CST8288
+* Professor: Reg Dyer
+* Lab Instructor: Leanne Seaward, Section 013
+* Project 1: Symphony Management System
+* Due: Friday, April 14, 2017
+*
+* Group Members:
+* Christopher Billings - bill0082@algonquinlive.com
+* Bryan Flood - floo0025@algonquinlive.com
+* Noah Guthrie - guth0025@algonquinlive.com
+* Zhongxuan Li - li000497@algonquinlive.com
+* Joel Schmuland - schm0111@algonquinlive.com
+*
+* // TODO
+* Class Info
+* 
+* @author Bryan Flood McAuley
+* @version 1.0 April 6, 2017
+*/
 public class Date {
 
 }
